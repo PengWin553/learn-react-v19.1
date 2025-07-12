5db69e1df65559e563d8315ad0638657dbeb905a
PengWin553 <victoriakobayashi553@gmail.com>
2025-07-12 20:56:54 +0800

    Create a React App with Synchronized Counters

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
-function App() {
+import React, { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleOnclick = () => {
+    setCount(count + 1);
+  }
 
   return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
+    <div className="column-alignment">
+      <Button count={count} onClick={handleOnclick} />
+      <Button count={count} onClick={handleOnclick} />
+    </div>
   )
+}
+
+const Button = ({count, onClick}) => {
+  return (
+    <button onClick={onClick}>
+      Count {count} times
+    </button>
+  );
 }
 
 export default App
