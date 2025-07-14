76dcc92f2c51443ef4b25e4a6103befff2f67f5f
PengWin553 <victoriakobayashi553@gmail.com>
2025-07-14 20:41:25 +0800

    PROPS - Adjust the image size based on a prop

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,30 @@
-function App() {
+import { getImageUrl } from './utils/utils.js';
+
+function Avatar({ person, size }) {
+  let thumbnailSize = 's';
+  if (thumbnailSize > 90) {
+    thumbnailSize = 'b'
+  }
 
   return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
-  )
+    <img
+      className="avatar"
+      src={getImageUrl(person, thumbnailSize)}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
+  );
 }
 
-export default App
+export default function App() {
+  return (
+    <Avatar
+      size={40}
+      person={{
+        name: 'Gregorio Y. Zara',
+        imageId: '7vQD0fP'
+      }}
+    />
+  );
+}
