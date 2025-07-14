5fd08a51b26b95911c49c54445504be2342184f9
PengWin553 <victoriakobayashi553@gmail.com>
2025-07-14 18:12:42 +0800

    Convert some HTML to JSX

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
-function App() {
+import React from 'react'
 
+const App = () => {
   return (
     <>
-      <h1>Hello, World!</h1>
-    </>
-  )
+      <div class="intro">
+        <h1>Welcome to my website!</h1>
+      </div>
+      <p class="summary">
+        You can find my thoughts here.
+        <br/>
+        
+        <b>And <i>pictures</i> of scientists! </b>
+      </p>
+    </> 
+  );
 }
 
 export default App
